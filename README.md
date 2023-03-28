0cc6cdacdad1e91a5a49d9e91bdb725ed6de16a1
Khanh Nguyen <khanhnv.uit@gmail.com>
2023-03-28 15:11:56 +0700

    add new handle to voice and update prompt for translate

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Requirments
 
 - python3.9
+
+## Installation
+
+1. clone the repository
+2. install dependencies `pip install -r requirements`
+3. make .env file `cp .env.example .env`
+4. store the `TELEGRAM_BOT_TOKEN` and `OPENAI_API_KEY` to .env
+5. start bot `python main.py`
+
--- main.py
+++ main.py
@@ -2,28 +2,34 @@ import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, CommandHandler, ContextTypes, filters
 import openai
+import whisper
 from dotenv import load_dotenv
 from pathlib import Path
-import ffmpeg
 
 load_dotenv()
 
 # Set up OpenAI API
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Define a function to handle incoming messages
+model = whisper.load_model('base.en', download_root="./.cached")
 
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+# Define a function to handle incoming messages to translate
+async def handle_translation(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Get the user's message
     message = update.message.text
 
-    # {"role": "system", "content": "You are a helpful assistant."},
+    message = message.replace("/translate", "")
+
     # Use the OpenAI API to generate a response
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0301",
         messages=[
             {"role": "system", "content": "You are a helpful, pattern-following assistant that translates corporate jargon into plain English."},
+            {"role": "user", "content": "can you translate for \"Bạn có thể tới đây sớm không?\" in English"},
+            {"role": "assistant", "content": "The translation for \"Bạn có thể tới đây sớm không?\" in English is \"Can you come here early?\"."},
+            {"role": "user", "content": "vòng đeo tay"},
+            {"role": "assistant", "content": "The translation for \"vòng đeo tay\" in English is \"bracelet\" or \"wristband\"."},
             {"role": "user", "content": message}],
         temperature=0,)
 
@@ -31,110 +37,56 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         chat_id=update.message.chat_id,
         text=response.choices[0].message.content,
         parse_mode='markdown')
-    
-async def translate_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    message = update.message.text
 
-    prompt = """
-        I want you to act as an English translator, spelling corrector and improver.
-        I will speak to you in any language and you will detect the language,
-        translate it and answer in the corrected and improved version of my text, in English.
-        I want you to replace my simplified A0-level words and sentences with more beautiful and elegant,
-        upper level English words and sentences. Keep the meaning same, but make them more literary.
-        I want you to only reply the correction, the improvements and nothing else,
-        do not write explanations.
-        My first sentence is \"{}\""""
-    
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt.format(message),
-    )
 
-    await context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=response.choices[0].text
-    )
+# Define a function to handle incoming message to ask everything
+async def handle_ask(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text
+    msg = msg.replace("/ask", "")
 
-async def handle_davinci(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Get the user's message
-    message = update.message.text
-
-    # {"role": "system", "content": "You are a helpful assistant."},
-    # Use the OpenAI API to generate a response
+    prompt = "As an AI language model, I have a vast amount of knowledge and can provide you with information on a wide range of topics. So, please feel free to ask me anything you want to know! Whether it's about science, history, geography, art, culture, or any other subject, I'm here to help you learn and expand your knowledge. So, go ahead and ask me any question you have in mind, and I'll do my best to provide you with a helpful answer."
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0301",
         messages=[
-            {"role": "system", "content": "You are a helpful, pattern-following assistant that translates corporate jargon into plain English."},
-            {"role": "user", "content": message}],
-        temperature=0,)
-
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": msg}
+        ]
+    )
     await context.bot.send_message(
         chat_id=update.message.chat_id,
-        text=response.choices[0].message.content,
-        parse_mode='markdown')
-    
+        text=response.choices[0].message.content
+    )
+
+
+# Define function to handle incoming voice to transcribe
 async def handle_audio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
-    print("function working...")
     voice = update.message.voice
 
     file = await context.bot.get_file(voice.file_id)
     oga = await file.download_to_drive()
 
-    # out = str(Path(oga)) + '.mp3'
-
-    # ffmpeg.input(oga).output(out, format='mp3').run(capture_stdout=True)
-
-    # f = open(out, 'rb')
-
-    # response = openai.Audio.transcribe('whisper-1', f)
-
-    # # remove cache file
-    # os.remove(oga)
-    # os.remove(out)
+    response = model.transcribe(str(Path(oga)), fp16=False)
 
     await context.bot.send_message(
         chat_id=update.message.chat_id,
-        text=response.text)
-    
-async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    mess = update.message
+        text=response['text'])
 
-    print(mess)
-    
-# def moderate_message(
-#     message: str, user: str
-# ) -> Tuple[str, str]:  # [flagged_str, blocked_str]
-#     moderation_response = openai.Moderation.create(
-#         input=message, model="text-moderation-latest"
-#     )
-#     category_scores = moderation_response.results[0]["category_scores"] or {}
-
-#     blocked_str = ""
-#     flagged_str = ""
-#     for category, score in category_scores.items():
-#         if score > MODERATION_VALUES_FOR_BLOCKED.get(category, 1.0):
-#             blocked_str += f"({category}: {score})"
-#             logger.info(f"blocked {user} {category} {score}")
-#             break
-#         if score > MODERATION_VALUES_FOR_FLAGGED.get(category, 1.0):
-#             flagged_str += f"({category}: {score})"
-#             logger.info(f"flagged {user} {category} {score}")
-#     return (flagged_str, blocked_str)
+    os.remove(oga)
 
 
+# main function
 def main() -> None:
     """Run bot."""
     # Create the Application and pass it your bot's token.
     bot = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
-    # application.add_handler(MessageHandler(filters.TEXT, handle_message))
-    bot.add_handler(CommandHandler('husky', handle_message))
-    bot.add_handler(CommandHandler('any2en', translate_prompt))
+    bot.add_handler(CommandHandler('translation', handle_translation))
+    bot.add_handler(CommandHandler("ask", handle_ask))
     bot.add_handler(MessageHandler(filters.VOICE, handle_audio))
-    # bot.add_handler(MessageHandler(filters.ALL, handler))
     bot.run_polling()
 
 
+# Run the script
 if __name__ == "__main__":
     print('bot started')
     main()
